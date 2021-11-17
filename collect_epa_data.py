654d462c95ae221677b2348f7fb9fd28847d81a1
CSULB Data Science Lab <66384937+csulb-datascience@users.noreply.github.com>
2021-11-17 14:54:48 -0800

    Update collect_epa_data.py

--- collect_epa_data.py
+++ collect_epa_data.py
@@ -2,22 +2,40 @@
 
 import requests
 import json
+import csv
 from epa_api import epa_api
 
 api = epa_api()
 
-# no, no2, so2, co, co2, o3, pm2.5,
+# NO, NO2, SO2, CO, CO2, PM10, PM2.5
+chemicals = ["42605", "42602", "42401", "42101", "42102", "81102" "88101"]
+times = ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00"]
 date = "20200101"
-times = ["06:00", "07:00", "08:00", "09:00", "10:00"]
-chemicals = ["44201"]
-sites = api.sites_list()
-for s in sites:
-    county = s["county_code"]
-    site = s["site_code"]
-    print(*api.get_data(county, site, date, times, chemicals), sep='\n')
 
-CA = "06"
-CO2 = "42102"
-NO2 = "42602"
-SO2 = "42401"
-CarbonPM10 = "82116"
+def sites_to_csv():
+    with open("listofEPAsites.csv", "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        header = ["STATE_CODE", "COUNTY_CODE", "COUNTY", "EPA ID", "SITE_LATITUDE", "SITE_LONGITUDE"]
+        csv_writer.writerow(header)
+        with open("siteCoordinates.json") as json_file:
+            data = json.load(json_file)
+            for d in data:
+                row = list(d.values())
+                csv_writer.writerow(row)
+            json_file.close()
+        csv_file.close()
+
+def collect_site_coordinates():
+    sites = api.sites_list()
+    site_data = api.get_site_coordinates(sites, chemicals)
+    print(*site_data, sep='\n')
+
+    with open("siteCoordinates.json", "w") as file:
+        json.dump(site_data, file, indent=2)
+        file.close()
+
+def test_api():
+    for s in sites:
+        county = s["county_code"]
+        site = s["site_code"]
+        print(*api.get_data(county, site, date, times chemicals), sep='\n')
